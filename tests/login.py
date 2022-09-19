4338bf5d362bad93226b346711eb7b77ee0ec97f
Oleg Dzyadovich <oleg.dzyadovich@biamp.com>
2022-09-19 15:47:24 -0700

    added tests file

--- tests/login.py
+++ tests/login.py
@@ -1,48 +1,75 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as chrome_options
-import allure
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import pytest
+import time
+from selenium.webdriver.chrome.options import Options
+from locators.locators import Locators as l
+
+options = Options()
 
 
-@pytest.fixture
-def get_chrome_options():
-    options = chrome_options()
-    options.add_argument('chrome')
-    options.add_argument("--start-maximized")
-
-    return options
+usernames = ("standard_user", "problem_user")
+link = "https://www.saucedemo.com/"
 
 
-@pytest.fixture
-def get_webdriver(get_chrome_options):
-    options = get_chrome_options
-    driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-    return driver
+class TestFirst:
+    def setup_method(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(5)
+        self.browser.get(link)
 
+    @pytest.mark.parametrize('users', usernames)
+    def test_successful_login(self, users):
+        username_field = self.browser.find_element(l.USERNAME_FIELD_XPATH)
+        username_field.send_keys(users)
+        time.sleep(2)
+        password_field = self.browser.find_element(l.PASSWORD_FIELD_CSS)
+        password_field.send_keys('secret_sauce')
 
-@pytest.fixture(scope='function')
-def setup(request, get_webdriver):
-    driver = get_webdriver
-    url = 'https://www.saucedemo.com/'
-    if request.cls is not None:
-        request.cls.driver = driver
-    driver.get(url)
-    yield driver
-    driver.quit()
+        button = self.browser.find_element(l.BUTTON_XPATH)
+        button.click()
+        time.sleep(2)
+        expected_text = 'PRODUCTS'
+        actual_text = self.browser.find_element(By.CLASS_NAME, 'title').text
 
+        assert actual_text == expected_text, f'Failed. Expected text is {expected_text}, but got {actual_text}'
 
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_makereport(item):
-    outcome = yield
-    rep = outcome.get_result()
-    marker = item.get_closest_marker("ui")
-    if marker:
-        if rep.when == "call" and rep.failed:
-            try:
-                allure.attach(item.instance.driver.get_screenshot_as_png(),
-                              name=item.name,
-                              attachment_type=allure.attachment_type.PNG)
-            except Exception as e:
-                print(e)
+    def test_unsuccessful_login(self):
+        username_field = self.browser.find_element(l.USERNAME_FIELD_XPATH)
+        username_field.send_keys('standard_user')
+        time.sleep(2)
+        password_field = self.browser.find_element(l.PASSWORD_FIELD_CSS)
+        password_field.send_keys('invalid')
+
+        button = self.browser.find_element(l.BUTTON_XPATH)
+        button.click()
+        time.sleep(2)
+        expected_text = "Epic sadface: Username and password do not match any user in this service"
+        actual_text = self.browser.find_element(By.XPATH, "//h3[@data-test='error']").text
+
+        assert actual_text == expected_text, f'Failed. Expected text is {expected_text}, but got {actual_text}'
+
+    def test_no_password_login(self):
+        username_field = self.browser.find_element(l.USERNAME_FIELD_XPATH)
+        username_field.send_keys('problem_user')
+        time.sleep(2)
+        button = self.browser.find_element(l.BUTTON_XPATH)
+        button.click()
+        expected = "Epic sadface: Password is required"
+        actual = self.browser.find_element(By.XPATH, "//h3[@data-test='error']").text
+        assert expected == actual
+
+    def test_locked_user(self):
+        username_field = self.browser.find_element(l.USERNAME_FIELD_XPATH)
+        username_field.send_keys('locked_out_user')
+        time.sleep(2)
+        password_field = self.browser.find_element(l.PASSWORD_FIELD_CSS)
+        password_field.send_keys('secret_sauce')
+        button = self.browser.find_element(l.BUTTON_XPATH)
+        button.click()
+        expected = "Epic sadface: Sorry, this user has been locked out."
+        actual = self.browser.find_element(By.XPATH, "//h3[@data-test='error']").text
+        assert expected == actual
+
+    def teardown_method(self):
+        self.browser.quit()
