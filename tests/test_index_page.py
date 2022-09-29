92c88ae4c327a22b28b61398898713919576af19
Oleg Dzyadovich <oleg.dzyadovich@biamp.com>
2022-09-29 14:05:59 -0700

    new commit

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,7 +1,16 @@
+import pytest
+import random
 
+@pytest.mark.ui
 def test_login(index_page):
     index_page.input_username()
     index_page.input_password()
     index_page.press_login_btn()
     index_page.assertion_products()
+
+'''random_numbers = [1,2,3]
+@pytest.mark.smoke
+def test_1():
+    assert 1 == random.choice(random_numbers)'''
+
 
