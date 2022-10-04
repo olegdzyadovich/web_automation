2e0f68e853415cb8764cae6788c415186bfca981
Oleg Dzyadovich <oleg.dzyadovich@biamp.com>
2022-10-04 14:14:06 -0700

    new commit

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,12 +1,21 @@
 import pytest
 import random
+import allure
 
+
+@allure.severity(allure.severity_level.BLOCKER)
+@allure.description("testing successful log in ")
+@allure.label("owner", "Oleg")
 @pytest.mark.ui
 def test_login(index_page):
-    index_page.input_username()
-    index_page.input_password()
-    index_page.press_login_btn()
-    index_page.assertion_products()
+    with allure.step("input valid username"):
+        index_page.input_username()
+    with allure.step("input valid pass"):
+        index_page.input_password()
+    with allure.step("press submit button"):
+        index_page.press_login_btn()
+    with allure.step("validate page title text"):
+        index_page.assertion_products()
 
 '''random_numbers = [1,2,3]
 @pytest.mark.smoke
